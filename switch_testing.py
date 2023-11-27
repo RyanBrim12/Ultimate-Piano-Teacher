46be3e26bcd9779367af8f66ab5b96c8b030f94d
RyanBrim12 <90427080+RyanBrim12@users.noreply.github.com>
2023-11-27 15:06:21 -0500

    Initial commit

---
+++ switch_testing.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import threading
+import pygame
+
+PINS = {
+    "c": 2
+  #  "c#": 3,
+  #  "d": 4,
+ #   "d#": 14,
+ #   "e": 15,
+  #  "f": 18,
+  #  "f#": 17,
+   # "g": 27,
+   # "g#": 22,
+   # "a": 23,
+   # "a#": 24,
+   # "b": 10
+}
+SOUND_FILENAMES = {
+    "a": "./sounds/a.wav",
+    "a#": "./sounds/a#.wav",
+    "b": "./sounds/b.wav",
+    "c": "./sounds/c.wav",
+    "c#": "./sounds/c#.wav",
+    "d": "./sounds/d.wav",
+    "d#": "./sounds/d#.wav",
+    "e": "./sounds/e.wav",
+    "f": "./sounds/f.wav",
+    "f#": "./sounds/f#.wav",
+    "g": "./sounds/g.wav",
+    "g#": "./sounds/g#.wav"
+}
+
+
+def play_note(file_name):
+   pygame.mixer.music.load(file_name)
+   pygame.mixer.music.play()
+   while pygame.mixer.music.get_busy():
+       continue
+
+
+def wait_for_press(note):
+    pin = PINS[note]
+    file_name = SOUND_FILENAMES[note]
+    while True:
+        if GPIO.input(pin) == GPIO.HIGH:
+            print(note)
+            play_note(file_name)
+
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    for pin in PINS.values():
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    pygame.mixer.init()
+
+    for note in PINS:
+        thread = threading.Thread(target=wait_for_press, args=(note,))
+        thread.start()
+
